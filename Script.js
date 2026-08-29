dfbb141924741cc6be38c68c73089dc3fd718ef3
rufino-guga <abgustta99@gmail.com>
2026-08-29 18:20:39 -0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -6,13 +6,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (Total) {
         Total.addEventListener("click", function () {
-            window.open("https://totalpass.com/br/academias/academia-tyson-gym/");
+            window.open("https://totalpass.com/br/academias/tk-academia/");
         });
     }
 
     if (Well) {
         Well.addEventListener("click", function () {
-            window.open("https://wellhub.com/pt-br/search/partners/academia-tyson-gym-jardim-do-cedro/");
+            window.open("https://wellhub.com/pt-br/search/partners/tk-academia-braganca-paulista/?utm_content=Consideration_Text_Keywords_Wellhub-Gyms&gad_source=1&gclid=Cj0KCQjwhsrUBhDxARIsAN3AQSf42tGxWswA0_pSW7dkENGCWfA0VC46eAN3Obce2Rizfbf8ONLYV40aAvb2EALw_wcB&utm_medium=paid&utm_campaign=latam-br_b2c_google-search-brand-partners-wellhub_con_ongoing_all_signup_0326&utm_source=google&utm_term=paid-academias%20com%20wellhub");
         });
     }
 
@@ -82,7 +82,7 @@ document.addEventListener("DOMContentLoaded", function () {
         btnInstagram.addEventListener("click", function () {
 
             window.open(
-                "https://www.instagram.com/academiatysongym/"
+                "https://www.instagram.com/tk_academia_dit/"
             );
 
         });
